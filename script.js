cf7055101e1c2e15c03984be882bd94ccd86883a
nad-15 <123393892+nad-15@users.noreply.github.com>
2025-01-21 22:34:11 +0800

    resizing styles

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.getElementById('date-container'); 
+appTitle = document.getElementById('app-title');
 
 for (let i = 0; i < 30; i++) {
     const div = document.createElement('div'); // Create a new div element
@@ -6,3 +7,4 @@ for (let i = 0; i < 30; i++) {
     div.innerText = `Div ${i + 1}`; 
     container.appendChild(div); 
 }
+console.log(window.getComputedStyle(appTitle).fontSize);
