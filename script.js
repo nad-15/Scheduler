79ca7fd5b9cc268c93dc8bac8f9a12bc87a8d308
nad-15 <123393892+nad-15@users.noreply.github.com>
2025-01-22 23:14:48 +0800

    adding divs dynamically for days

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
 const monthContainer = document.getElementById('month-container');
-const daysContainer = document.getElementById('days-container');
+// const daysContainer = document.getElementById('days-container');
 // appTitle = document.getElementById('app-title');
 
+const today = new Date();
+const day = today.getDay();
+const date = today.getDate();
+const month = today.getMonth();
+const year = today.getFullYear();
 
-function cloneDaysContainer() {
-    for (let i = 0; i < 30; i++) {
-        const clonedDaysContainer = daysContainer.cloneNode(true);
-        monthContainer.appendChild(clonedDaysContainer);
-        console.log('cloned');
+console.log(day);
+console.log(date);
+console.log(month);
+console.log(year);
+
+const currentCalendarDate = new Date(year, month + 2, 0);
+const firstDayOfMonth = currentCalendarDate.getDate();
+
+console.log(firstDayOfMonth);
+
+function addDays(date = "1", day = "Sun", morningTask = "", afternoonTask = "", eveningTask = "") {
+
+    for (i = 1; i < 30; i++) {
+        const dayContainer = document.createElement('div');
+        dayContainer.classList.add('day-container');
+
+        const dateDiv = document.createElement('div');
+        dateDiv = classList.add('date');
+        dateDiv.textContent = date;
+
+        const dayDiv = createElement('div');
+        dayDiv = classList.add('day');
+        dayDiv.textContent = day;
+
+        const morningTaskDiv = document.createElement('div');
+        morningTaskDiv.classList.add('morning');
+        morningTaskDiv.textContent = morningTask;
+
+        const afternoonTaskDiv = document.createElement('div');
+        afternoonTaskDiv.classList.add('afternoon');
+        afternoonTaskDiv.textContent = morningTask;
+
+        const eveningTaskDiv = document.createElement('div');
+        eveningTaskDiv.classList.add('evening');
+        eveningTaskDiv.textContent = morningTask;
+
+
+        dayContainer.appendChild(dateDiv);
+        dayContainer.appendChild(dayDiv);
+        dayContainer.appendChild(morningTaskDiv);
+        dayContainer.appendChild(afternoonTaskDiv);
+        dayContainer.appendChild(eveningTaskDiv);
+
+        monthContainer.appendChild(dayContainer);
+
     }
 }
 
-cloneDaysContainer();
-
-
-// for (let i = 0; i < 30; i++) {
-//     const div = document.createElement('div'); // Create a new div element
-//     div.classList.add('days');
-//     div.innerText = `Div ${i + 1}`;
-//     container.appendChild(div);
-// }
-// console.log(window.getComputedStyle(appTitle).fontSize);
