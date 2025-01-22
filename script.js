2b5d5a7bd614f57b3f6f78f6f59d6716bba8cfc1
nad-15 <123393892+nad-15@users.noreply.github.com>
2025-01-22 10:02:38 +0800

    styling

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const container = document.getElementById('date-container'); 
-appTitle = document.getElementById('app-title');
+// const container = document.getElementById('date-container'); 
+// appTitle = document.getElementById('app-title');
 
-for (let i = 0; i < 30; i++) {
-    const div = document.createElement('div'); // Create a new div element
-    div.classList.add('days'); 
-    div.innerText = `Div ${i + 1}`; 
-    container.appendChild(div); 
-}
-console.log(window.getComputedStyle(appTitle).fontSize);
+
+
+// for (let i = 0; i < 30; i++) {
+//     const div = document.createElement('div'); // Create a new div element
+//     div.classList.add('days'); 
+//     div.innerText = `Div ${i + 1}`; 
+//     container.appendChild(div); 
+// }
+// console.log(window.getComputedStyle(appTitle).fontSize);
