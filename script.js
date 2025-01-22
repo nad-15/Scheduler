b24f9ea489a9fb16777ccf8b2f748051b6a5e917
nad-15 <123393892+nad-15@users.noreply.github.com>
2025-01-22 14:35:45 +0800

    styling days divs flex

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-// const container = document.getElementById('date-container'); 
+const monthContainer = document.getElementById('month-container');
+const daysContainer = document.getElementById('days-container');
 // appTitle = document.getElementById('app-title');
 
+
+function cloneDaysContainer() {
+    for (let i = 0; i < 30; i++) {
+        const clonedDaysContainer = daysContainer.cloneNode(true);
+        monthContainer.appendChild(clonedDaysContainer);
+        console.log('cloned');
+    }
+}
+
+cloneDaysContainer();
 
 
 // for (let i = 0; i < 30; i++) {
 //     const div = document.createElement('div'); // Create a new div element
-//     div.classList.add('days'); 
-//     div.innerText = `Div ${i + 1}`; 
-//     container.appendChild(div); 
+//     div.classList.add('days');
+//     div.innerText = `Div ${i + 1}`;
+//     container.appendChild(div);
 // }
 // console.log(window.getComputedStyle(appTitle).fontSize);
