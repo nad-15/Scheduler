8ed5e76384095201ebc31f6756dbfd083a5a0571
nullp01nt3r <123393892+nad-15@users.noreply.github.com>
2026-09-18 23:25:05 +0800

    style: increase list-view open icon size and opacity and bump cache version

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Skhayeduler Service Worker - Network-First Strategy (Zero Stale Code)
 // ==========================================================================
 
-const CACHE_NAME = 'scheduler-pwa-v119';
+const CACHE_NAME = 'scheduler-pwa-v120';
 
 const STATIC_ASSETS = [
     './',
